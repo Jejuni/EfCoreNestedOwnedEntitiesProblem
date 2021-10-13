4c6d6a3816711ad13220c24a71c92cc5d4213e49
JejuniProgramming <jejuniprogramming@gmail.com>
2021-10-13 17:53:57 +0200

    added migrations

---
+++ EfCoreNestedOwnedEntitiesProblem/Migrations/20211013155332_InitialMigration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EfCoreNestedOwnedEntitiesProblem.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EntityOnes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MyProp_LvlTwo_LvlThree_MyInteger = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntityOnes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EntityTwos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MyOtherProp_LvlTwo_LvlThree_MyInteger = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntityTwos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntityOnes");
+
+            migrationBuilder.DropTable(
+                name: "EntityTwos");
+        }
+    }
+}
---
+++ EfCoreNestedOwnedEntitiesProblem/Migrations/20211013155342_NoChangeMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EfCoreNestedOwnedEntitiesProblem.Migrations
+{
+    public partial class NoChangeMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyOtherProp_LvlTwo_LvlThree_LvlTwoLvlOneEntityOneId",
+                table: "EntityTwos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MyProp_LvlTwo_LvlThree_MyInteger",
+                table: "EntityOnes",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProp_LvlTwo_LvlThree_MyInteger",
+                table: "EntityOnes");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "MyOtherProp_LvlTwo_LvlThree_LvlTwoLvlOneEntityOneId",
+                table: "EntityTwos",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+    }
+}
